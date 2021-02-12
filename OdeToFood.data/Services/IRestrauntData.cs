64fef4a51aa5be9052cf2efccaaf61ffd954bde7
Jacob Hensley <jhensley@nirvc.com>
2021-02-12 14:02:11 -0600

    Created InMemoryRestrauntDaa for mock Data of restraunts

--- OdeToFood.data/Services/IRestrauntData.cs
+++ OdeToFood.data/Services/IRestrauntData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OdeToFood.data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace OdeToFood.data.Services
 {
     public interface IRestrauntData
     {
-        IEnumerable<Restraunt>
+        IEnumerable<Restraunt> GetAll();
     }
 }
---
+++ OdeToFood.data/Services/InMemoryRestrauntData.cs
@@ -0,0 +1,29 @@
+﻿using OdeToFood.data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.data.Services
+{
+    public class InMemoryRestrauntData : IRestrauntData
+    {
+
+        List<Restraunt> restraunts;
+
+        public InMemoryRestrauntData()
+        {
+            restraunts = new List<Restraunt>()
+            {
+                new Restraunt { Id = 1, Name = "Scott's Pizza", Cuisine = CuisineType.Italian},
+                new Restraunt { Id = 2, Name = "Tersiguels", Cuisine = CuisineType.French},
+                new Restraunt { Id = 3, Name = "Mango Grove", Cuisine = CuisineType.Indian},
+            };
+        }
+        public IEnumerable<Restraunt> GetAll()
+        {
+            return restraunts.OrderBy(r => r.Name);
+        }
+    }
+}
